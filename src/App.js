61655605a01a8d5cc2fd99fd88450855ea90d0cb
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-06-14 03:55:57 +0530

    app component changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-import LoginPage from './components/LoginPage';
+import { HomePage } from './components/HomePage';
+import { LoginPage } from './components/LoginPage';
+import { DashboardPage } from './components/DashboardPage';
+import { AddEventPage } from './components/AddEventPage';
+import { RegisterPage } from './components/RegisterPage';
 
 function App() {
   return (
-    <div>
-      <LoginPage />
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/register">
+          <RegisterPage />
+        </Route>
+        <Route exact path="/dashboard">
+          <DashboardPage />
+        </Route>
+        <Route exact path="/add">
+            <AddEventPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
